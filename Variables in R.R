9f0f7f896580b7aa68a479be3db58e3b57771038
Sumit4509 <104025799+Sumit4509@users.noreply.github.com>
2024-02-17 01:32:26 +0530

    Adding a file of Variables in R

---
+++ Variables in R.R
@@ -0,0 +1,26 @@
+#variables in R programming 
+#In R programming dot(.) can be used in variable 
+#Varibale name can be start from character,dot, but it can not start with underscore or number
+#Except dot and underscore no other special symbol is allowed in naming of variable
+
+
+#below onesis valid 
+var_name <- 10 
+var.name <- 30
+.var_name <- 20
+var5name <-92
+
+#now below ones can not be allowed 
+_var_name<- 50
+var_name*<-20
+5var_name<- 604
+
+
+#Assignment of operators
+#there are three type of assignment of operators -  <- , = ,-> 
+
+var1<- 30
+var2= 40
+10-> var3
+
+#but prefarable is <- to use 
